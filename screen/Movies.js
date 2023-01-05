c75dd53117f52320e56ec7f97a83e01eae421234
kimtanook <seob6402@gmail.com>
2023-01-05 21:23:37 +0900

    Delete unnecessary

--- screen/Movies.js
+++ screen/Movies.js
@@ -1,16 +1,6 @@
 import styled from "@emotion/native";
-import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  ImageBackground,
-  RefreshControl,
-  ScrollView,
-  Text,
-  View,
-} from "react-native";
+import { ActivityIndicator, FlatList, ScrollView, View } from "react-native";
 import Swiper from "react-native-swiper";
 
 import NowPlaying from "../components/NowPlaying";
@@ -18,99 +8,86 @@ import TopRated from "../components/TopRated";
 import UpComing from "../components/UpComing";
 
 export default function Movies() {
-  const [nowPlaying, setNowPlaying] = useState([]);
-  const [topRated, setTopRated] = useState([]);
-  const [upComing, setUpComing] = useState([]);
-
-  const [isRefreshing, setItRefreshing] = useState(false);
+  const [nowPlayingData, setNowPlaying] = useState([]);
+  const [topRatedData, setTopRated] = useState([]);
+  const [upComingData, setUpComing] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isRefreshing, setItRefreshing] = useState(false);
 
   const getNowPlayingMovies = async () => {
-    try {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
-      ).then((res) => res.json());
-      setNowPlaying(response.results);
-      setIsLoading(false);
-    } catch (error) {
-      console.log("error : ", error);
-    }
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
+    ).then((res) => res.json());
+    setNowPlaying(response.results);
   };
   const getTopRatedMovies = async () => {
-    try {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/top_rated?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
-      ).then((res) => res.json());
-      setTopRated(response.results);
-      setIsLoading(false);
-    } catch (error) {
-      console.log("error : ", error);
-    }
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
+    ).then((res) => res.json());
+    setTopRated(response.results);
   };
   const getUpComingMovies = async () => {
-    try {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/upcoming?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
-      ).then((res) => res.json());
-      setUpComing(response.results);
-      setIsLoading(false);
-    } catch (error) {
-      console.log("error : ", error);
-    }
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
+    ).then((res) => res.json());
+    setUpComing(response.results);
   };
 
+  const onRefreshing = async () => {
+    setItRefreshing(true);
+    await getData();
+    setItRefreshing(false);
+  };
   const getData = async () => {
     await Promise.all([
       getNowPlayingMovies(),
       getTopRatedMovies(),
       getUpComingMovies(),
     ]);
-  };
-  const onRefreshing = async () => {
-    setItRefreshing(true);
-    await getData();
-    setItRefreshing(false);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  if (isLoading) {
-    return (
-      <View>
-        <ActivityIndicator />
-      </View>
-    );
-  }
-
   return (
-    <FlatList
-      refreshing={isRefreshing}
-      onRefresh={onRefreshing}
-      ListHeaderComponent={
-        <View style={{ flex: 1 }}>
-          <ListTitle>Now Playing</ListTitle>
-          <Swiper height={300} showsPagination={false} autoplay loop>
-            {nowPlaying.map((movie) => (
-              <NowPlaying key={movie.id} movie={movie} />
-            ))}
-          </Swiper>
-          <ListTitle>Top Rated</ListTitle>
-          <FlatList
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            data={topRated}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <TopRated movie={item} />}
-          />
-          <ListTitle>Up Coming</ListTitle>
+    <>
+      {isLoading ? (
+        <View>
+          <ActivityIndicator />
         </View>
-      }
-      data={upComing}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <UpComing movie={item} />}
-    />
+      ) : (
+        <>
+          <FlatList
+            refreshing={isRefreshing}
+            onRefresh={onRefreshing}
+            ListHeaderComponent={
+              <>
+                <ListTitle>Now Playing</ListTitle>
+                <Swiper height={300} showsPagination={false} autoplay loop>
+                  {nowPlayingData.map((movie) => (
+                    <NowPlaying key={movie.id} movie={movie} />
+                  ))}
+                </Swiper>
+                <ListTitle>Top Rated</ListTitle>
+                <FlatList
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  data={topRatedData}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({ item }) => <TopRated movie={item} />}
+                />
+                <ListTitle>Up Coming</ListTitle>
+              </>
+            }
+            data={upComingData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <UpComing movie={item} />}
+          />
+        </>
+      )}
+    </>
   );
 }
 
