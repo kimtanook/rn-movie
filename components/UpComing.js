4bac2e34e15f3b455ed6828aece6ad031cb1af31
kimtanook <seob6402@gmail.com>
2023-01-05 19:37:56 +0900

    Update UpComing component refactoring

--- components/UpComing.js
+++ components/UpComing.js
@@ -10,88 +10,51 @@ import {
   View,
 } from "react-native";
 
-const UpComing = () => {
-  const [upComing, setUpComing] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const getUpComingMovies = async () => {
-    try {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/upcoming?api_key=3cc2f8f41df6043f8268cd5597421998&language=en-US&page=1"
-      ).then((res) => res.json());
-      setUpComing(response.results);
-      setIsLoading(false);
-    } catch (error) {
-      console.log("error : ", error);
-    }
-  };
-
-  useEffect(() => {
-    getUpComingMovies();
-  }, []);
+const UpComing = ({ movie }) => {
   return (
-    <>
-      {isLoading ? (
-        <StLoadingView>
-          <ActivityIndicator />
-        </StLoadingView>
-      ) : (
-        <StUnderItemWrapView>
-          {upComing.map((movie) => (
-            <StUnderItemView>
-              <StUnderItemImage
-                source={{
-                  uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-                }}
-                style={{
-                  resizeMode: "cover",
-                }}
-              />
-              <StTitleBodyView>
-                <StUnderItemTitle>{movie.title}</StUnderItemTitle>
-                <StUnderItemBody numberOfLines={4} ellipsizeMode="tail">
-                  {movie.overview}
-                </StUnderItemBody>
-              </StTitleBodyView>
-            </StUnderItemView>
-          ))}
-        </StUnderItemWrapView>
-      )}
-    </>
+    <StView>
+      <StImage
+        source={{
+          uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+        }}
+        style={{
+          resizeMode: "cover",
+        }}
+      />
+      <StTextView>
+        <StTitleText>{movie.title}</StTitleText>
+        <StText numberOfLines={4} ellipsizeMode="tail">
+          {movie.overview}
+        </StText>
+      </StTextView>
+    </StView>
   );
 };
 
 export default UpComing;
 
-const StLoadingView = styled.View`
-  flex: 1;
-  justify-content: center;
-  height: 300px;
-`;
-const StUnderItemWrapView = styled.View`
-  flex: 1;
-  margin: 10px;
-`;
-const StUnderItemView = styled.View`
-  margin: 10px;
-  padding: 10px;
+const StView = styled.View`
   flex: 1;
   flex-direction: row;
+  margin: 10px;
   height: 200px;
   width: 100%;
 `;
-const StUnderItemImage = styled.Image`
+
+const StImage = styled.Image`
   flex: 1;
   height: 100%;
 `;
-const StTitleBodyView = styled.View`
+const StTextView = styled.View`
   flex: 2;
-  margin-left: 10px;
+  flex-direction: column;
+  justify-content: flex-end;
 `;
-const StUnderItemTitle = styled.Text`
-  flex: 1;
+const StTitleText = styled.Text`
   color: white;
+  font-size: 20px;
+  margin-bottom: 10px;
 `;
-const StUnderItemBody = styled.Text`
-  flex: 1;
+const StText = styled.Text`
   color: white;
 `;
